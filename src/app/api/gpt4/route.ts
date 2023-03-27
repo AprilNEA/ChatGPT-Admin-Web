17c9581ede1a48647c4b87478fb9988cbb178c5a
AprilNEA <github@sku.moe>
2023-03-28 00:57:23 +0800

    feat: gpt4 support

---
+++ src/app/api/gpt4/route.ts
@@ -0,0 +1,62 @@
+import {NextRequest} from "next/server";
+import {askGPT} from '@/lib/***REMOVED***/gpt4'
+
+
+export async function POST(req: NextRequest) {
+  try {
+    const {messages, max_tokens, temperature} = await req.json();
+    const conversations = messages.map(
+      // @ts-ignore
+      ({role, content}) => ({role, text: content}),
+    );
+
+    const stream = readableStreamFromIterable(await askGPT({
+      conversations,
+      system_message: "You are ChatGPT based on GPT-4.",
+    }, {max_tokens, temperature, cookie: process.env.***REMOVED***!}))
+
+    const headers = new Headers();
+    headers.append("Content-Type", "text/event-stream");
+    headers.append("Cache-Control", "no-cache");
+    headers.append("Connection", "keep-alive");
+
+
+    return new Response(stream, {headers})
+  } catch (error) {
+    console.error("[Chat Stream]", error);
+  }
+}
+
+export const config = {
+  runtime: "edge",
+};
+
+// copied from deno/std
+function readableStreamFromIterable(
+  iterable: Iterable<string> | AsyncIterable<string>,
+): ReadableStream<Uint8Array> {
+  const iterator: Iterator<string> | AsyncIterator<string> =
+    (iterable as AsyncIterable<string>)[Symbol.asyncIterator]?.() ??
+    (iterable as Iterable<string>)[Symbol.iterator]?.();
+
+  const encoder = new TextEncoder()
+
+  return new ReadableStream({
+    async pull(controller) {
+      const {value, done} = await iterator.next();
+      if (done) {
+        controller.close();
+      } else {
+        controller.enqueue(encoder.encode(value));
+      }
+    },
+    async cancel(reason) {
+      if (typeof iterator.throw == "function") {
+        try {
+          await iterator.throw(reason);
+        } catch { /* `iterator.throw()` always throws on site. We catch it. */
+        }
+      }
+    },
+  });
+}
