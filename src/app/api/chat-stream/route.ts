06ae2b7ccde4629ee6110a9688ac32094c6dcaa4
peron <peron_ap@icloud.com>
2023-03-29 22:35:06 +0800

    feat: use signal from request

--- src/app/api/chat-stream/route.ts
+++ src/app/api/chat-stream/route.ts
@@ -1,28 +1,6 @@
 import { createParser } from "eventsource-parser";
 import { NextRequest } from "next/server";
 
-async function fetchDataWithTimeout(url:string, options:RequestInit, timeout = 30000) {
-  const abortController = new AbortController();
-  const signal = abortController.signal;
-
-  // 设置超时
-  const timeoutId = setTimeout(() => {
-    abortController.abort();
-  }, timeout);
-
-  try {
-    const res = await fetch(url, { ...options, signal });
-    clearTimeout(timeoutId); // 清除超时，因为请求已完成
-    return res;
-    // @ts-ignore
-  } catch (error) {// @ts-ignore
-    if (error.name === 'AbortError') {
-      throw new Error('Request timed out');
-    }
-    throw error;
-  }
-}
-
 async function createStream(req: NextRequest) {
   const encoder = new TextEncoder();
   const decoder = new TextDecoder();
@@ -35,14 +13,18 @@ async function createStream(req: NextRequest) {
     console.log("[Stream] using user api key");
   }
 
-  const res = await fetchDataWithTimeout("https://api.openai.com/v1/chat/completions", {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${apiKey}`,
+  const res = await fetch(
+    "https://api.openai.com/v1/chat/completions",
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiKey}`,
+      },
+      method: "POST",
+      body: req.body,
+      signal: req.signal,
     },
-    method: "POST",
-    body: req.body,
-  });
+  );
 
   return new ReadableStream({
     async start(controller) {
