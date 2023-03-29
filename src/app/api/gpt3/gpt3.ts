ea3f1e89841d9647eebc437b64a31c61c41cf2e4
AprilNEA <github@sku.moe>
2023-03-30 04:01:16 +0800

    fix(lex-ai): git3 ai ask add model assign

--- src/app/api/gpt3/gpt3.ts
+++ src/app/api/gpt3/gpt3.ts
@@ -1,11 +1,12 @@
 export class LexGPT {
-  constructor(private cookie: string, private max_tokens = 128) {}
+  constructor(private cookie: string, private max_tokens = 500) {}
 
   private static apiURL = "https://***REMOVED***.page/ai/ask";
 
   private getForm(text: string): FormData {
     const form = new FormData();
     form.append("text", text);
+    form.append("gpt_model", "gpt-3.5-turbo");
     form.append("max_tokens", this.max_tokens.toString());
     return form;
   }
