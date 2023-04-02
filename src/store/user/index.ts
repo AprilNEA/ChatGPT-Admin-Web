e09e4c889d7ba9690637f6b3f247cedde71395c4
AprilNEA <github@sku.moe>
2023-04-02 11:22:17 +0800

    feat: add Version for announcement and move type back

--- src/store/user/index.ts
+++ src/store/user/index.ts
@@ -1,34 +1,49 @@
-import {create} from "zustand";
-import {persist} from "zustand/middleware";
-import type {UserStore} from "@/types/user";
-import type {Cookie} from "@/lib/redis/typing";
-import {cookies} from "next/headers";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import type { Cookie } from "@/lib/redis/typing";
 
 const LOCAL_KEY = "user-store";
 
+export interface UserStore {
+  cookie: Cookie | null;
+  email: string;
+  versionId: string;
+  updateVersionId: (versionId: string) => void;
+  updateEmail: (email: string) => void;
+  updateCookie: (cookie: Cookie) => void;
+  validateCookie: () => boolean;
+}
+
 export const useUserStore = create<UserStore>()(
-  persist((set, get) => ({
+  persist(
+    (set, get) => ({
       cookie: null,
-      email: '',
+      email: "",
+      versionId: "",
+
+      updateVersionId(versionId: string) {
+        set((state) => ({ versionId }));
+      },
 
       updateEmail(email: string) {
-        set((state) => ({email}));
+        set((state) => ({ email }));
       },
 
       updateCookie(cookie: Cookie) {
-        set((state) => ({cookie}));
+        set((state) => ({ cookie }));
       },
 
       /**
        * 检验 Cookie 是否过期
        */
       validateCookie() {
-        const cookie = get().cookie
+        const cookie = get().cookie;
         return !!(cookie && cookie.exp > Date.now());
-      }
+      },
     }),
     {
       name: LOCAL_KEY,
       version: 1,
-    })
-)
+    }
+  )
+);
--- src/types/user.ts
+++
@@ -1,9 +0,0 @@
-import {Cookie} from "@/lib/redis/typing";
-
-export interface UserStore {
-  cookie: Cookie | null;
-  email: string;
-  updateEmail: (email: string) => void;
-  updateCookie: (cookie: Cookie) => void;
-  validateCookie: () => boolean;
-}
