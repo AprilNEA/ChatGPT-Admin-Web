d1bb3db4edf05e54d92d72c98c3fcb9782c1766f
peron <peron_ap@icloud.com>
2023-04-05 04:13:49 +0800

    feat: addPaymentLog()

---
+++ packages/dal/src/models/audit.ts
@@ -0,0 +1,29 @@
+import { redis } from 'src/redis/client';
+import { Model } from './typing';
+
+export class AuditDAL {
+  private ip: string;
+  private userEmail: string;
+
+  constructor({ ip, userEmail }: Model.AuditLog) {
+    this.ip = ip;
+    this.userEmail = userEmail;
+  }
+
+  async addPaymentLog(
+    tradeOrderId: string,
+    log: Omit<Model.PaymentAudioLog, keyof Model.AuditLog>
+  ): Promise<boolean> {
+    const paymentLog: Model.PaymentAudioLog = {
+      ip: this.ip,
+      userEmail: this.userEmail,
+      timestamp: Date.now(),
+      ...log,
+    };
+
+    return (
+      (await redis.hmset(`auditLog:payment:${tradeOrderId}`, paymentLog)) ===
+      'OK'
+    );
+  }
+}
