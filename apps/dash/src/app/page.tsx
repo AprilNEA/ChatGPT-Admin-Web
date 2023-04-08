6ad5bbed56b94769489f10289995bb60cfd440da
AprilNEA <github@sku.moe>
2023-04-08 19:21:58 +0800

    chore: init dash apps

---
+++ apps/dash/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main>
+    </main>
+  )
+}
