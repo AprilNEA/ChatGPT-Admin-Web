bd1075e00bbba3d913791171e4c41bfac76fa316
AprilNEA <github@sku.moe>
2023-04-11 02:46:27 +0800

    feat: update notice 20230410

--- apps/chat/src/utils/requests.ts
+++ apps/chat/src/utils/requests.ts
@@ -101,7 +101,7 @@ export async function requestChatStream(
   const reqTimeoutId = setTimeout(() => controller.abort(), TIME_OUT_MS);
 
   try {
-    const res = await fetch("/api/chat-stream", {
+    const res = await fetch("/api/gpt4", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
