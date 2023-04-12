28dccd36162cea86da689b5c33ba2cf6990b9143
peron <peron_ap@icloud.com>
2023-04-12 23:21:09 +0800

    feat: lib to handle bing requests in browser

---
+++ apps/chat/src/lib/bing/request.ts
@@ -0,0 +1,73 @@
+import {
+  BingGeneratorEvent,
+  POSTBody,
+  RecordedMessage,
+  sendMessageHandlers,
+} from "./types";
+import { TextLineStream } from "./utils";
+
+const API_ENDPOINT = "https://bing.p1xl.me/chat";
+
+function sendMessageAndGetStream(
+  body: POSTBody,
+): Promise<ReadableStream<Uint8Array>> {
+  return fetch(API_ENDPOINT, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  }).then((res) => res.body!);
+}
+
+async function* streamToEventGenerator(
+  stream: ReadableStream<Uint8Array>,
+): AsyncGenerator<BingGeneratorEvent> {
+  const lineStream = stream
+    .pipeThrough(new TextDecoderStream()) // only work in browser
+    .pipeThrough(new TextLineStream());
+
+  const reader = lineStream.getReader();
+
+  while (true) {
+    const { done, value: line } = await reader.read();
+    if (done) break;
+
+    if (line.startsWith("data:")) {
+      yield JSON.parse(line.slice("data:".length));
+    }
+  }
+
+  reader.releaseLock();
+}
+
+export function sendMessageGenerator(
+  body: POSTBody,
+): Promise<AsyncGenerator<BingGeneratorEvent>> {
+  return sendMessageAndGetStream(body).then(streamToEventGenerator);
+}
+
+export async function sendMessage(
+  body: POSTBody,
+  { onQuery, onAnswer, onReset, onDone, onError }: sendMessageHandlers,
+) {
+  for await (const event of await sendMessageGenerator(body)) {
+    switch (event.type) {
+      case "ANSWER":
+        onAnswer(event.answer);
+        break;
+      case "QUERY":
+        onQuery(event.query);
+        break;
+      case "RESET":
+        onReset(event.text);
+        break;
+      case "DONE":
+        onDone(event.text);
+        break;
+      case "ERROR":
+        onError(event.error);
+        break;
+    }
+  }
+}
---
+++ apps/chat/src/lib/bing/types.ts
@@ -0,0 +1,35 @@
+export type RecordedMessage = {
+  author: "user" | "bot";
+  text: string;
+};
+
+export type BingGeneratorEvent = {
+  type: "DONE";
+  text: string;
+} | {
+  type: "ANSWER";
+  answer: string;
+} | {
+  type: "ERROR";
+  error: unknown;
+} | {
+  type: "RESET";
+  text: string;
+} | {
+  type: "QUERY";
+  query: string;
+};
+
+export interface POSTBody {
+  userMessage: string;
+  cookie: string;
+  history?: RecordedMessage[];
+}
+
+export interface sendMessageHandlers {
+  onQuery: (query: string) => void;
+  onAnswer: (answer: string) => void;
+  onReset: (text: string) => void;
+  onDone: (text: string) => void;
+  onError: (error: unknown) => void;
+}
