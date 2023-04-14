65b1cc1554f88db93a051776d94cd9e9bed75471
AprilNEA <github@sku.moe>
2023-04-14 12:18:14 +0800

    fix: spelling errors

--- apps/chat/src/components/banner/index.tsx
+++ apps/chat/src/components/banner/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '@/styles/module/Banner.module.scss';
+import styles from '@/styles/module/banner.module.scss';
 import Image from 'next/image'
 import Link from "next/link";
 import BannerImage from "@/assets/banner.png";
