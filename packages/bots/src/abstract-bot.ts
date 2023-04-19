513cdccba82c039ff3b97bec528724ac1d651f5c
peron <peron_ap@icloud.com>
2023-04-20 00:12:21 +0800

    fix: should ignore user abort exception

--- packages/bots/src/abstract-bot.ts
+++ packages/bots/src/abstract-bot.ts
@@ -3,7 +3,15 @@ import { AnswerParams } from "./types";
 export abstract class AbstractBot {
   protected abstract doAnswer(params: AnswerParams): AsyncIterable<string>;
 
-  answer(params: AnswerParams): AsyncIterable<string> {
-    return this.doAnswer(params);
+  async *answer(params: AnswerParams): AsyncIterable<string> {
+    try {
+      for await (const token of this.doAnswer(params)) yield token;
+    } catch (error) {
+      if (!params.signal?.aborted) {
+        console.error("[BOT]", error);
+        yield "[ERROR]";
+      }
+      // ignore user abort exception
+    }
   }
 }
