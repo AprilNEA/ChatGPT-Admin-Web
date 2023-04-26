8763bd13c450c67449926edcd497af4de8244143
AprilNEA <github@sku.moe>
2023-04-26 22:56:35 +0800

    feat: analyze data

---
+++ apps/dash/src/app/api/analyze/[range]/route.ts
@@ -0,0 +1,32 @@
+import {NextRequest, NextResponse} from "next/server";
+
+import {UserDAL} from "database";
+
+type Range = "yesterday" | "total";
+
+export async function GET(req: NextRequest, {params}: {
+  params: { range: Range }
+}) {
+  //
+  switch (params.range){
+    case "yesterday":
+      break
+    case "total":
+      break
+    default:
+      break
+
+  }
+  return NextResponse.json({
+    status: 0,
+    data: {
+      user: 0,
+      paid: null, // [plan_name]: count
+      order: 0 // including pending
+    },
+  });
+}
+
+export const config = {
+  runtime: "edge",
+};
