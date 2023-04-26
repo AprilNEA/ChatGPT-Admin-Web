5b6afa35cc1fec3e4237fe46411dac9a7fc1d1fb
peron <peron_ap@icloud.com>
2023-04-26 23:45:25 +0800

    fix: rename to better reflect

--- packages/database/src/logic/apiKeyPool.ts
+++ packages/database/src/logic/apiKeyPool.ts
@@ -3,7 +3,7 @@ import { APIKeyPoolDAL } from "../dal";
 export class APIKeyPoolLogic {
   constructor(private readonly dal = new APIKeyPoolDAL()) {}
 
-  async getNewKey(): Promise<string | null> {
+  async getOneKey(): Promise<string | null> {
     const key = await this.dal.shift();
     if (key) {
       await this.dal.push(key);
