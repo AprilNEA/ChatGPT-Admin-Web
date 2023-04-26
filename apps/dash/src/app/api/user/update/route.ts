855073897ad95d35ea881156bd28551e6d6a0c97
AprilNEA <github@sku.moe>
2023-04-26 22:47:10 +0800

    feat: list and query users and orders

--- apps/dash/src/app/api/user/update/route.ts
+++ apps/dash/src/app/api/user/update/route.ts
@@ -2,19 +2,19 @@ import {NextRequest, NextResponse} from "next/server";
 
 import {UserDAL} from "database";
 
-export async function GET(req: NextRequest) {
+export async function POST(req: NextRequest) {
   const params = req.nextUrl.searchParams;
   const cursor = Number(params.get("cursor")) ?? 0;
   const userDal = new UserDAL()
-  const [count, users] = await userDal.listValues(cursor)
+  // const [count, users] = await userDal.listValues(cursor)
 
-  return NextResponse.json({
-    status: 0,
-    data: {
-      count,
-      users
-    },
-  });
+  // return NextResponse.json({
+  //   status: 0,
+  //   data: {
+  //     count,
+  //     users
+  //   },
+  // });
 }
 
 export const config = {
