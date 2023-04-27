9e1dd932917befd1fed6092b6ba7f7f57f93d60b
AprilNEA <github@sku.moe>
2023-04-27 18:51:00 +0800

    perf: optimize payment processing logic

--- apps/chat/src/lib/pay/xunhu.ts
+++ apps/chat/src/lib/pay/xunhu.ts
@@ -1,9 +1,10 @@
-import { fetch } from "@edge-runtime/ponyfill";
+import {fetch} from "@edge-runtime/ponyfill";
 import md5 from "spark-md5";
 import {type NextRequest} from "next/server";
 
 const appId = process.env.PAY_APPID!;
 const appSecret = process.env.PAY_APPSECRET!;
+const wapName = process.env.PAY_WAPNAME ?? "店铺名称";
 
 interface PaymentArgs {
   version: string;
@@ -46,7 +47,12 @@ export interface CallbackBody {
   hash?: string;
 }
 
-function sortAndSignParameters(parameters: PaymentArgs): string {
+/**
+ * Sort the key names and link together
+ * @param parameters
+ * @return linked sting
+ */
+function sortAndSignParameters(parameters: PaymentArgs | CallbackBody): string {
   // 过滤空值参数
   const filteredParameters = Object.entries(parameters).filter(
     ([, value]) => value !== null
@@ -65,23 +71,32 @@ function sortAndSignParameters(parameters: PaymentArgs): string {
   return stringA;
 }
 
-export async function startPay(orderId: string, price: number, email:string) {
+/**
+ * Request a order
+ * @param orderId internal order id
+ * @param price the price need to be paid
+ * @param attach encrypted field being transmitted.
+ * @param title payment title
+ */
+export async function startPay({orderId, price, attach, title}: {
+  orderId: string, price: number, attach: string, title?: string,
+}) {
   const fetchBody: PaymentArgs = {
     version: "1.1",
     appid: appId,
     trade_order_id: orderId,
     total_fee: price,
-    title: "仁爱路111号",
+    title: title ?? "ChatGPT-April-Web",
     time: Math.floor(Date.now() / 1000),
     notify_url: "https://new.lmo.best/api/user/pay/callback",
-    return_url: "https://lmo.best",
-    // callback_url: string;
+    return_url: "https://lmo.best", // After the user has successfully made the payment, we will automatically redirect the user's browser to this URL.
+    callback_url: "https://lmo.best", // After the user cancels the payment, we may guide the user to redirect to this URL to make the payment again.
     // plugins: string;
-    // attach: email, // 我们将 Email 附加在其中
-    nonce_str: "123",
+    attach, // Return as is during callback. 📢We use it to confirm that the order has not been tampered with.
+    nonce_str: orderId, // 1. Avoid server page caching 2. Prevent security keys from being guessed
     type: "WAP",
     wap_url: "https://lmo.best",
-    wap_name: "仁爱路",
+    wap_name: wapName,
   };
   const stringA = sortAndSignParameters(fetchBody);
   const hash = md5.hash(stringA + appSecret);
@@ -90,7 +105,7 @@ export async function startPay(orderId: string, price: number, email:string) {
     await fetch("https://api.xunhupay.com/payment/do.html", {
       cache: 'no-store',
       method: "POST",
-      headers: { "Content-Type": "application/json" },
+      headers: {"Content-Type": "application/json"},
       body: JSON.stringify({
         ...fetchBody,
         hash,
