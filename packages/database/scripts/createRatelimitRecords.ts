ec20dc2f1b2888068558a712b14d9cba37e19b04
peron <peron_ap@icloud.com>
2023-04-28 00:05:10 +0800

    feat(ratelimit): check remaining times and clear

--- packages/database/scripts/createRatelimitRecords.ts
+++ packages/database/scripts/createRatelimitRecords.ts
@@ -5,11 +5,17 @@ import { testRedis } from "../tests/client";
   const limiter = keywordRateLimiterOf({
     prefix: "test",
     limit: 10,
-    window: "5m",
+    window: "30s",
     redis: testRedis,
   });
 
   console.log(await limiter.limit("me"));
+  await delay(1000);
   console.log(await limiter.limit("me"));
+  await delay(1000);
   console.log(await limiter.limit("me"));
 })();
+
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
