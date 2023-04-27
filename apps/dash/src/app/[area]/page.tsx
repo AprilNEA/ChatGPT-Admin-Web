5207e3e554891dcccd1cb49fc281ad0560375048
AprilNEA <github@sku.moe>
2023-04-27 19:20:46 +0800

    chore(dash): remove unused type

--- apps/dash/src/app/[area]/page.tsx
+++ apps/dash/src/app/[area]/page.tsx
@@ -1,32 +1,31 @@
 "use client";
 
 import useSWR from "swr";
-import {ChangeEvent, useState} from "react";
-import {Table, Tag, Button, Form, Input} from "antd";
+import { ChangeEvent, useState } from "react";
+import { Table, Tag, Button, Form, Input } from "antd";
 import fetcher from "@/utils/fetcher";
-import {User} from "database";
 
-type Area = "user" | "order"
-const {Search} = Input;
+type Area = "user" | "order";
+const { Search } = Input;
 
 const getPlanTag = (plan: string) => {
   switch (plan) {
     case "free":
-      return <Tag color="gray">Free</Tag>
+      return <Tag color="gray">Free</Tag>;
     case "pro":
-      return <Tag color="green">Pro</Tag>
+      return <Tag color="green">Pro</Tag>;
     case "premium":
-      return <Tag color="gold">Premium</Tag>
+      return <Tag color="gold">Premium</Tag>;
     default:
-      return <Tag color="yellow">Unknown</Tag>
+      return <Tag color="yellow">Unknown</Tag>;
   }
-}
+};
 
 const orderColumns = [
   {
     title: "订单号",
     dataIndex: "order",
-    key: "order"
+    key: "order",
   },
   {
     title: "创建时间",
@@ -37,13 +36,13 @@ const orderColumns = [
   {
     title: "用户邮箱",
     dataIndex: "email",
-    key: "email"
+    key: "email",
   },
   {
     title: "计划",
     dataIndex: "plan",
     key: "plan",
-    render: (plan: string) => getPlanTag(plan)
+    render: (plan: string) => getPlanTag(plan),
   },
   {
     title: "状态",
@@ -52,27 +51,27 @@ const orderColumns = [
     render: (status: string) => {
       switch (status) {
         case "pending":
-          return <Tag color="gold">Pending</Tag>
+          return <Tag color="gold">Pending</Tag>;
         case "paid":
-          return <Tag color="green">Paid</Tag>
+          return <Tag color="green">Paid</Tag>;
         default:
-          return <Tag color="red">Failed</Tag>
+          return <Tag color="red">Failed</Tag>;
       }
-    }
+    },
   },
   {
     title: "订单金额",
     dataIndex: "totalCents",
-    key: "totalCents"
-  }
-]
+    key: "totalCents",
+  },
+];
 
 const userColumns = [
   {
     title: "姓名",
     dataIndex: "name",
     key: "name",
-    render: (text: string, {isBlocked}: { isBlocked: boolean }) => (
+    render: (text: string, { isBlocked }: { isBlocked: boolean }) => (
       <a>
         {text + " "}
         {isBlocked ? <Tag color="red">已封禁</Tag> : null}
@@ -83,7 +82,7 @@ const userColumns = [
     title: "订阅",
     dataIndex: "subscriptions",
     key: "subscriptions",
-    render: (_: any, {subscriptions}: { subscriptions: any }) =>
+    render: (_: any, { subscriptions }: { subscriptions: any }) =>
       subscriptions ? (
         <Tag color="green">已订阅</Tag>
       ) : (
@@ -115,44 +114,43 @@ const userColumns = [
   },
 ];
 
-export default function Page({params}: { params: { area: Area } }) {
-  let columns
+export default function Page({ params }: { params: { area: Area } }) {
+  let columns;
   switch (params.area) {
     case "user":
-      columns = userColumns
-      break
+      columns = userColumns;
+      break;
     case "order":
-      columns = orderColumns
-      break
+      columns = orderColumns;
+      break;
     default:
-      return <p>404</p>
+      return <p>404</p>;
   }
-  const {data, isLoading} = useSWR(
+  const { data, isLoading } = useSWR(
     `/api/${params.area}/list` as string,
-    (url) =>
-      fetcher(url).then((res) => res.json())
+    (url) => fetcher(url).then((res) => res.json())
   );
 
   const [searchEmail, setSearchEmail] = useState("");
 
   function handleChange(e: ChangeEvent<HTMLInputElement>) {
     setSearchEmail(e.target.value);
-    console.log(e.target.value)
+    console.log(e.target.value);
   }
 
   function handlerSubmit(e: SubmitEvent) {
-    console.log(searchEmail)
+    console.log(searchEmail);
   }
 
-  if (isLoading) return <p>isLoading</p>
+  if (isLoading) return <p>isLoading</p>;
   return (
     <>
       <Form
         name="basic"
-        labelCol={{span: 8}}
-        wrapperCol={{span: 16}}
-        style={{maxWidth: 600}}
-        initialValues={{remember: true}}
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        initialValues={{ remember: true }}
         onFinish={handlerSubmit}
         // onFinishFailed={onFinishFailed}
         autoComplete="off"
@@ -160,19 +158,19 @@ export default function Page({params}: { params: { area: Area } }) {
         <Form.Item
           label="Email"
           name="Email"
-          rules={[{required: false, message: "Please input your username!"}]}
+          rules={[{ required: false, message: "Please input your username!" }]}
         >
-          <Input value={searchEmail} onChange={handleChange}/>
+          <Input value={searchEmail} onChange={handleChange} />
         </Form.Item>
 
-        <Form.Item wrapperCol={{offset: 8, span: 16}}>
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
           <Button type="primary" htmlType="submit">
             Search
           </Button>
         </Form.Item>
       </Form>
       {/*@ts-ignore*/}
-      <Table dataSource={data?.data?.data} columns={columns}/>
+      <Table dataSource={data?.data?.data} columns={columns} />
     </>
   );
 }
