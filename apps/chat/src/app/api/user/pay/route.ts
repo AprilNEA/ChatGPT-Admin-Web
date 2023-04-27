07bd26ce4e7dfacb1e713bd059399736b2e1cde6
AprilNEA <github@sku.moe>
2023-04-27 19:04:30 +0800

    refactor: use subscription slots

--- apps/chat/src/app/api/user/pay/route.ts
+++ apps/chat/src/app/api/user/pay/route.ts
@@ -1,42 +1,42 @@
-import { startPay } from "@/lib/pay/xunhu";
-import { NextRequest, NextResponse } from "next/server";
-import { ResponseStatus } from "@/app/api/typing.d";
-import { OrderLogic } from "database";
-import type { Plan } from "database";
+import {startPay} from "@/lib/pay/xunhu";
+import {NextRequest, NextResponse} from "next/server";
+import {ResponseStatus} from "@/app/api/typing.d";
+import {OrderLogic, PlanDAL} from "database";
 
 export async function GET(req: NextRequest) {
   const email = req.headers.get("email")!; // pass from middleware
 
-  const { searchParams } = new URL(req.url);
-  const plan = searchParams.get("plan") as Plan
-  const cycle = searchParams.get("cycle")
+  const {searchParams} = new URL(req.url);
+  const plan = searchParams.get("plan")?.toLowerCase()
+  const cycle = searchParams.get("cycle")?.toLowerCase()
 
   if (!email || !plan || !cycle) {
-    return NextResponse.json({ status: ResponseStatus.Failed });
+    return NextResponse.json({status: ResponseStatus.Failed});
   }
-
-  // FIXME Get price from database
-  let price;
-  switch (plan.trim().toLowerCase()) {
-    case "pro":
-      price = 15;
-      break;
-    case "premium":
-      price = 99;
-      break;
-    default:
-      return NextResponse.json({ status: ResponseStatus.Failed });
-  }
+  // TODO use zod here
+  if (!["monthly", "quarterly", "yearly"].includes(cycle))
+    return NextResponse.json({status: ResponseStatus.Failed});
+  const planDal = new PlanDAL()
+  const planPrices = await planDal.readProperty(plan, "prices");
+  if (!planPrices) // TODO Refine error types
+    return NextResponse.json({status: ResponseStatus.Failed}); // @ts-ignore
+  const totalPrices = planPrices[cycle]
 
   const order = new OrderLogic()
   const orderId = await order.newOrder({
     createdAt: Date.now(),
-    totalCents: price,
+    totalCents: totalPrices,
     plan,
-    count,
+    count: 1,
     status: "pending",
     email,
   });
+
   if (!orderId) throw Error("new order failed");
-  return NextResponse.json(await startPay(orderId, price, email));
+  return NextResponse.json(await startPay({
+    orderId: orderId,
+    price: totalPrices,
+    title: `${plan} ${cycle}`,
+    attach: "",
+  }));
 }
