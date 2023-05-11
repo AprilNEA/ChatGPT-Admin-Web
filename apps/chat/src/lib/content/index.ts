09260e515a7f5cdceb9f1c123afee204d57355bf
AprilNEA <github@sku.moe>
2023-05-12 03:42:22 +0800

    fix(chat): prevent environment variables from not being set

--- apps/chat/src/lib/content/index.ts
+++ apps/chat/src/lib/content/index.ts
@@ -1,10 +1,12 @@
-import { tencentTextSecurity } from "@/lib/content/tencent";
-import { baiduTextSecurity } from "@/lib/content/baidu";
+import {tencentTextSecurity} from "@/lib/content/tencent";
+import {baiduTextSecurity} from "@/lib/content/baidu";
 
-const service = process.env.TEXT_SECURITY ?? "";
+const service = process.env.TEXT_SECURITY;
 
 export async function textSecurity(conversation: any) {
   /* If the secure text service is not set up.*/
+  if (!service) return true;
+
   switch (service?.toLowerCase()) {
     case "baidu":
       return await baiduTextSecurity(JSON.stringify(conversation));
