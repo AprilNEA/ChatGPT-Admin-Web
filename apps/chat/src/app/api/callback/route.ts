a6df5aa1438bbbb6d4e746f5fdbb44f1dab79393
Lusk Ridder <1402709211@qq.com>
2023-05-31 17:51:20 +0000

    修修复了付费后无论是monthly 还是quartly 还是yearly，截止日期都是一个月的时间的bug

--- apps/chat/src/app/api/callback/route.ts
+++ apps/chat/src/app/api/callback/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest } from "next/server";
 import { handleCallback } from "@/lib/pay/xunhu";
 import { OrderLogic, SubscriptionLogic } from "database";
+import { prices } from "../../(chat)/pricing/page"
 
 /**
  * This is the callback interface for processing payment platforms.
@@ -16,6 +17,18 @@ export async function POST(req: NextRequest) {
   // Modify order status.
   const order = await orderLogic.getOrder(orderId);
 
+  const totolMoney = order!.totalCents;
+
+  let numberOfDay= 1;
+
+  if (totolMoney == prices[1].price.monthly || totolMoney == prices[2].price.monthly) {
+    numberOfDay = 30;
+  } else if (totolMoney == prices[1].price.quarterly || totolMoney == prices[2].price.quarterly) {
+    numberOfDay = 90;
+  } else {
+    numberOfDay = 365;
+  }
+
   if (order?.status === "pending") {
     await orderLogic.updateStatus(orderId, "paid");
   }
@@ -25,7 +38,7 @@ export async function POST(req: NextRequest) {
   const subscriptionLogic = new SubscriptionLogic();
   await subscriptionLogic.append(order!.email, {
     startsAt: Date.now(),
-    endsAt: Date.now() + 1000 * 60 * 60 * 24 * 30,
+    endsAt: Date.now() + 1000 * 60 * 60 * 24 * numberOfDay,
     plan: order!.plan,
     tradeOrderId: orderId,
   });
