92b29fe267c24bbf5a6ad5447bb187db50010f94
AprilNEA <github@sku.moe>
2023-10-24 00:09:28 +0800

    feat: add health check handler

--- packages/backend/src/app.controller.ts
+++ packages/backend/src/app.controller.ts
@@ -1,11 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from '@/common/guards/auth.guard';
 
+@Public()
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/health')
+  health() {
+    const packageJson = require('../../../package.json');
+
+    return {
+      version: packageJson.version,
+    };
+  }
+
+  @Post('/install')
   getHello(): string {
     return this.appService.getHello();
   }
