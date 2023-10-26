a53d62b62f558023775368b39006ba3b1d10d853
AprilNEA <github@sku.moe>
2023-10-26 22:12:04 +0800

    feat: add api for get info and change name

--- packages/backend/src/modules/user/user.service.ts
+++ packages/backend/src/modules/user/user.service.ts
@@ -5,7 +5,35 @@ import { DatabaseService } from '@/processors/database/database.service';
 export class UserService {
   constructor(private prisma: DatabaseService) {}
 
-  async getInfo(userId: number) {}
+  async getInfo(userId: number) {
+    const user = this.prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+      select: {
+        name: true,
+        email: true,
+        phone: true,
+      },
+    });
+    return user;
+  }
+
+  async updateName(userId: number, name: string) {
+    const user = await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        name,
+      },
+      select: {
+        name: true,
+      },
+    });
+    return user;
+  }
+
   async getSettings(userId: number) {
     return this.prisma.chatSetting.findUnique({
       where: {
