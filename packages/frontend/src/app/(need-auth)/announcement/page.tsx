f267a769619fa49913f6364c5d43e1c42a4b87ab
AprilNEA <github@sku.moe>
2023-11-06 23:16:18 +0800

    fix!: make build successfully

--- packages/frontend/src/app/(need-auth)/announcement/page.tsx
+++ packages/frontend/src/app/(need-auth)/announcement/page.tsx
@@ -4,49 +4,49 @@ import { useRouter } from 'next/navigation';
 import useSWR, { useSWRConfig } from 'swr';
 
 import { IconButton } from '@/components/button';
+import { Markdown } from '@/components/markdown';
 import { List, ListItem } from '@/components/ui-lib';
+import { Loading } from '@/components/ui-lib';
 import CloseIcon from '@/icons/close.svg';
 import Locale, { changeLang, getLang } from '@/locales';
-import { Markdown } from '@/components/markdown';
-import styles from '@/styles/module/announcement.module.scss';
 import { useStore } from '@/store';
-import { Loading } from '@/components/ui-lib';
+import styles from '@/styles/module/announcement.module.scss';
 
-export default function AnnouncePage(){
-    const router = useRouter();
-    const {fetcher} = useStore();
-    const { data: announcement, isLoading: isAnnouncementLoading } = useSWR(
-        '/announcement/all',
-        (url) =>
-        fetcher(url)
-            .then((res) => res.json())
-            .then((res) => res.data),
-        {
-        keepPreviousData: true,
-        revalidateOnFocus: false,
-        },
-    );
+export default function AnnouncePage() {
+  const router = useRouter();
+  const { fetcher } = useStore();
+  const { data: announcement, isLoading: isAnnouncementLoading } = useSWR(
+    '/announcement/all',
+    (url) =>
+      fetcher(url)
+        .then((res) => res.json())
+        .then((res) => res.data),
+    {
+      keepPreviousData: true,
+      revalidateOnFocus: false,
+    },
+  );
 
-    function DateFormat(time: string | number): string {
-        const date = new Date(time);
-    
-        const day: string = ('0' + date.getDate()).slice(-2);
-        const month: string = ('0' + (date.getMonth() + 1)).slice(-2);
-        const year: number = date.getFullYear();
-    
-        const hours: string = ('0' + date.getHours()).slice(-2);
-        const minutes: string = ('0' + date.getMinutes()).slice(-2);
-        const seconds: string = ('0' + date.getSeconds()).slice(-2);
-    
-        const formattedDate: string = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-        return formattedDate;
-      }
+  function DateFormat(time: string | number): string {
+    const date = new Date(time);
 
-    if (isAnnouncementLoading) return <Loading />;
+    const day: string = ('0' + date.getDate()).slice(-2);
+    const month: string = ('0' + (date.getMonth() + 1)).slice(-2);
+    const year: number = date.getFullYear();
 
-    return (
-        <>
-        <div className={styles['window-header']}>
+    const hours: string = ('0' + date.getHours()).slice(-2);
+    const minutes: string = ('0' + date.getMinutes()).slice(-2);
+    const seconds: string = ('0' + date.getSeconds()).slice(-2);
+
+    const formattedDate: string = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    return formattedDate;
+  }
+
+  if (isAnnouncementLoading) return <Loading />;
+
+  return (
+    <>
+      <div className={styles['window-header']}>
         <div className={styles['window-header-title']}>
           <div className={styles['window-header-main-title']}>
             {Locale.Announcement.Title}
@@ -65,33 +65,34 @@ export default function AnnouncePage(){
       </div>
       <div className={styles['announcement']}>
         <div className={styles['announcement-coloum']}>
-            {announcement ? (
-            announcement.map((announce) => (
-                <>
-                    <div className={styles['announcement-header']}>
-                        <div>{`#${announce.id}`}</div>
-                        <div className={styles['announcement-date']}>
-                            {DateFormat(announce.updatedAt)}
-                        </div>
-                    </div>
-                    <List>
-                        <ListItem>
-                            <div className={styles['announce-body']}>
-                                <div className={styles['title']}>{announce.title}</div>
-                                <Markdown content={announce.content}></Markdown>
-                            </div>
-                        </ListItem>
-                    </List>
-                </>
-            ))) : (
+          {announcement ? (
+            announcement.map((announce: any) => (
+              <>
+                <div className={styles['announcement-header']}>
+                  <div>{`#${announce.id}`}</div>
+                  <div className={styles['announcement-date']}>
+                    {DateFormat(announce.updatedAt)}
+                  </div>
+                </div>
                 <List>
-                    <ListItem>
-                        <>{Locale.Announcement.NoEntry}</>
-                    </ListItem>
+                  <ListItem>
+                    <div className={styles['announce-body']}>
+                      <div className={styles['title']}>{announce.title}</div>
+                      <Markdown content={announce.content}></Markdown>
+                    </div>
+                  </ListItem>
                 </List>
-              )}
+              </>
+            ))
+          ) : (
+            <List>
+              <ListItem>
+                <>{Locale.Announcement.NoEntry}</>
+              </ListItem>
+            </List>
+          )}
         </div>
       </div>
-        </>
-    )
+    </>
+  );
 }
