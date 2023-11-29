890ca7c99d054b6152530adca5fc2317a4f669ef
AprilNEA <github@sku.moe>
2023-11-30 00:59:26 +0800

    docs: show copy code default; add flex search

---
+++ docs/theme.config.tsx
@@ -0,0 +1,13 @@
+export default {
+  logo: <span>ChatGPT Admin Web</span>,
+  project: {
+    link: 'https://github.com/ArilNEA/ChatGPT-Admin-Web',
+  },
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
+    toggleButton: true,
+  },
+  toc: {
+    backToTop: true,
+  },
+};
