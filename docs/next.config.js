eb09ade589836ede6a742116f9544bc731c4f980
AprilNEA <github@sku.moe>
2023-11-30 00:17:26 +0800

    chore: update docs next config

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,6 +1,6 @@
-const withNextra = require("nextra")({
-  theme: "nextra-theme-docs",
-  themeConfig: "./theme.config.jsx",
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx',
 });
 
 module.exports = withNextra({
