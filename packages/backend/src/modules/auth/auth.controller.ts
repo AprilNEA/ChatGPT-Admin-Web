5b402a04724e4a6c813bf5a6dcdb6795eac79551
AprilNEA <github@sku.moe>
2023-12-03 21:12:52 +0800

    fix(backend): fix identity schema and use upper camel case instead of lower camel case

--- packages/backend/src/modules/auth/auth.controller.ts
+++ packages/backend/src/modules/auth/auth.controller.ts
@@ -16,14 +16,14 @@ import { WechatService } from '@/modules/auth/wechat.service';
 import { forgetPasswordDto, identityDto, validateCodeDto } from 'shared';
 
 import {
+  ForgetPasswordSchema,
+  InitAdminSchema,
+  InitPasswordSchema,
+  PasswordLoginSchema,
+  PasswordSchema,
+  RequireCodeSchema,
+  ValidateCodeSchema,
   bindIdentitySchema,
-  forgetPasswordSchema,
-  identitySchema,
-  initAdminSchema,
-  initPasswordSchema,
-  passwordSchema,
-  validateCodeSchema,
-  withPasswordSchema,
 } from './auth.dto';
 import { AuthService } from './auth.service';
 
@@ -35,7 +35,7 @@ export class AuthController {
   ) {}
 
   @Public()
-  @UsePipes(new JoiValidationPipe(initAdminSchema))
+  @UsePipes(new JoiValidationPipe(InitAdminSchema))
   @Post('admin/setup')
   async initAdmin(@Body() data: { identity: string; password: string }) {
     await this.authService.initAdmin(data.identity, data.password);
@@ -46,7 +46,7 @@ export class AuthController {
 
   /* 方法一：密码登录 */
   @Public()
-  @UsePipes(new JoiValidationPipe(withPasswordSchema))
+  @UsePipes(new JoiValidationPipe(PasswordLoginSchema))
   @Post('password')
   async password(@Body() data: { identity: string; password: string }) {
     return {
@@ -60,14 +60,14 @@ export class AuthController {
   @Public()
   @Get('validateCode')
   async newValidateCode(
-    @Query(new JoiValidationPipe(identitySchema)) query: identityDto,
+    @Query(new JoiValidationPipe(RequireCodeSchema)) query: identityDto,
   ) {
     return await this.authService.newValidateCode(query.identity);
   }
 
   /** 2.通过验证码登录/注册，自动识别邮箱或手机号 */
   @Public()
-  @UsePipes(new JoiValidationPipe(validateCodeSchema))
+  @UsePipes(new JoiValidationPipe(ValidateCodeSchema))
   @Post('validateCode')
   async loginByCode(@Body() data: validateCodeDto) {
     return {
@@ -100,7 +100,7 @@ export class AuthController {
   /* 忘记密码 */
   @Public()
   @Post('forgetPassword')
-  @UsePipes(new JoiValidationPipe(forgetPasswordSchema))
+  @UsePipes(new JoiValidationPipe(ForgetPasswordSchema))
   async forgetPassword(@Body() data: forgetPasswordDto) {
     await this.authService.forgetPassword(
       data.identity,
@@ -116,7 +116,7 @@ export class AuthController {
   @Put('changePassword')
   async changePassword(
     @Payload('id') userId: number,
-    @Body('password', new JoiValidationPipe(passwordSchema)) password: string,
+    @Body('password', new JoiValidationPipe(PasswordSchema)) password: string,
   ) {
     await this.authService.changePassword(userId, password);
     return {
@@ -127,7 +127,7 @@ export class AuthController {
   @Put('initUsername')
   async initName(
     @Payload('id') userId: number,
-    @Body(new JoiValidationPipe(initPasswordSchema))
+    @Body(new JoiValidationPipe(InitPasswordSchema))
     body: { username: string },
   ) {
     await this.authService.initUsername(userId, body.username);
@@ -140,7 +140,7 @@ export class AuthController {
   @Put('initPassword')
   async initPassword(
     @Payload('id') userId: number,
-    @Body(new JoiValidationPipe(initPasswordSchema))
+    @Body(new JoiValidationPipe(InitPasswordSchema))
     body: { password: string },
   ) {
     await this.authService.initPassword(userId, body.password);
--- packages/backend/src/modules/auth/auth.dto.ts
+++ packages/backend/src/modules/auth/auth.dto.ts
@@ -3,45 +3,42 @@ import * as Joi from 'joi';
 const email = Joi.string().email();
 const phone = Joi.string().pattern(/^[0-9]{11}$/);
 
-const _identitySchema = Joi.alternatives().try(email, phone).required();
-export const passwordSchema = Joi.string().min(8).max(20).required();
+export const IdentitySchema = Joi.alternatives().try(email, phone).required();
+export const PasswordSchema = Joi.string().min(8).max(20).required();
 
-export const initUsernameSchema = Joi.object({
-  username: Joi.string().min(2).max(12).optional(),
+export const RequireCodeSchema = Joi.object({
+  identity: IdentitySchema,
 });
 
-export const identitySchema = Joi.object({
-  identity: _identitySchema,
-});
-
-export const validateCodeSchema = Joi.object({
-  identity: Joi.alternatives().try(email, phone).required(),
+export const ValidateCodeSchema = Joi.object({
+  identity: IdentitySchema,
   code: Joi.string()
     .pattern(/^[0-9]{6}$/)
     .required(),
 });
 
-export const initPasswordSchema = Joi.object({
-  password: passwordSchema.required(),
+export const InitPasswordSchema = Joi.object({
+  password: PasswordSchema.required(),
 });
 
-export const forgetPasswordSchema = Joi.object({
+export const ForgetPasswordSchema = Joi.object({
   identity: Joi.alternatives().try(email, phone).required(),
   code: Joi.string()
     .pattern(/^[0-9]{6}$/)
     .required(),
-  newPassword: passwordSchema.required(),
+  newPassword: PasswordSchema.required(),
 });
 
-export const withPasswordSchema = Joi.object({
-  password: passwordSchema.required(),
+export const PasswordLoginSchema = Joi.object({
+  identity: IdentitySchema,
+  password: PasswordSchema.required(),
 });
 
 export const bindIdentitySchema = Joi.object({
-  identity: Joi.alternatives().try(email, phone).required(),
+  identity: IdentitySchema,
 });
 
-export const initAdminSchema = Joi.object({
-  identity: _identitySchema,
-  password: passwordSchema.required(),
+export const InitAdminSchema = Joi.object({
+  identity: IdentitySchema,
+  password: PasswordSchema.required(),
 });
