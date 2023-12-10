5e2641f9e17a4d8a9bdaf203d3df71f32d810250
AprilNEA <github@sku.moe>
2023-12-10 17:00:35 +0800

    chore(vitest): make expend declare successfully

--- test/setup-file.ts
+++ test/setup-file.ts
@@ -1,8 +1,34 @@
 import { expect } from 'vitest';
+import type { Assertion, AsymmetricMatchersContaining } from 'vitest';
+import { ZodError } from 'zod';
 
-import { toBeSafe } from '@test/schema';
+export function toBeSafe<T extends any>(
+  received: { success: true; data: T; error?: ZodError },
+  expected?: T,
+) {
+  // @ts-ignore
+  const { isNot } = this;
+  return {
+    // 请勿根据 isNot 参数更改你的 "pass" 值，Vitest 为你做了这件事情
+    pass:
+      expected !== undefined
+        ? received.success && received.data === expected
+        : received.success,
+    message: () => `${received.data} is${isNot ? ' not' : ''} safe`,
+  };
+}
 
 expect.extend({ toBeSafe });
+
+interface CustomMatchers<R = unknown> {
+  toBeSafe(): R;
+}
+
+declare module 'vitest' {
+  interface Assertion<T = any> extends CustomMatchers<T> {}
+
+  interface AsymmetricMatchersContaining extends CustomMatchers {}
+}
 
 // import { redisHelper } from './helper/redis-mock.helper'
 // import { prisma } from './lib/prisma'
