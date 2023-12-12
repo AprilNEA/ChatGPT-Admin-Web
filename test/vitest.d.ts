8995522fbff77390df522b03930e8f78cb025bfa
AprilNEA <github@sku.moe>
2023-12-12 10:07:36 +0800

    chore: remove unused pkg, interop vitest

--- test/vitest.d.ts
+++
@@ -1,9 +0,0 @@
-interface CustomMatchers<R = unknown> {
-  toBeFoo(): R;
-}
-
-declare module 'vitest' {
-  interface Assertion<T = any> extends CustomMatchers<T> {}
-
-  interface AsymmetricMatchersContaining extends CustomMatchers {}
-}
