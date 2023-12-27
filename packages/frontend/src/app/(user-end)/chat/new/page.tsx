d0a5418f35897ea9fb34e101cccb800eaf45968a
AprilNEA <github@sku.moe>
2023-12-27 17:45:54 +0800

    fix: crypto.randomUUID is not a function in some browser edge browser case Appliction error: a client-side exception.

--- packages/frontend/src/app/(user-end)/chat/new/page.tsx
+++ packages/frontend/src/app/(user-end)/chat/new/page.tsx
@@ -4,6 +4,8 @@
    移除将会导致在客户端未刷新的情况下 randomUUID 永远相同 */
 import { RedirectType, redirect } from 'next/navigation';
 
+import { randomUUID } from '@/utils/client-utils';
+
 export default function NewChatPage() {
-  redirect(`/chat/${crypto.randomUUID()}?new=true`, RedirectType.replace);
+  redirect(`/chat/${randomUUID()}?new=true`, RedirectType.replace);
 }
